c115efdf1b46500fcfffc030ad302d492fea3fd7
Leonardo <leonardorecco3@gmail.com>
2024-02-26 08:40:35 -0300

    First Commit

---
+++ extra-junit/src/Calculadora.java
@@ -0,0 +1,3 @@
+public class Calculadora {
+    private Double resultado;
+}
---
+++ extra-junit/src/Pessoa.java
@@ -0,0 +1,25 @@
+public class Pessoa {
+
+private String nome;
+private Double peso;
+    public Pessoa(String nome, Double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
